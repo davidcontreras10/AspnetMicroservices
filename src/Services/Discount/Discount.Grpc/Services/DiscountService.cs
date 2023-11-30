3159c58223106a0332e62fe63e7241b6bf42bf74
David C <david.contreras@gorillalogic.com>
2023-11-30 11:38:59 -0600

    latest course

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,35 @@
+﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Services
+{
+	public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+	{
+		private readonly IDiscountRepository _discountRepository;
+		private readonly ILogger<DiscountService> _logger;
+
+		public DiscountService(IDiscountRepository discountRepository, ILogger<DiscountService> logger)
+		{
+			_discountRepository = discountRepository;
+			_logger = logger;
+		}
+
+		public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+		{
+			var coupon = await _discountRepository.GetDiscount(request.ProductName);
+			if (coupon == null)
+			{
+				throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName={request.ProductName} is not found."));
+			}
+
+			_logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
+
+			var couponModel = _mapper.Map<CouponModel>(coupon);
+			return couponModel;
+		}
+
+	}
+}
